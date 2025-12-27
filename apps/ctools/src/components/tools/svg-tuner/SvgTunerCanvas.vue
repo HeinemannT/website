03dd06ed1c19eb4d5788bf26d3f15fcfec7409c2
Tassilo Heinemann <tassilo.heinemann@stud.th-rosenheim.de>
2025-12-27 19:41:13 +0100

    fxs

--- apps/ctools/src/components/tools/svg-tuner/SvgTunerCanvas.vue
+++ apps/ctools/src/components/tools/svg-tuner/SvgTunerCanvas.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import { Minus, Plus, Maximize, Sun, Moon, Grid, Monitor } from 'lucide-vue-next'
 
 const props = defineProps<{
